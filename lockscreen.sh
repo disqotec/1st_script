fa18918be9cf1415ddefd9933e3e2e9e4980cc66
disqotec <disqo@outlook.com>
2019-02-15 15:01:38 -0800

    FINALLY!!

--- lockscreen.sh
+++ lockscreen.sh
@@ -6,7 +6,7 @@ mkdir ~/Documents/lockscreen
 touch ~/Documents/lockscreen/lock
 
 echo "!/usr/bin/env bash" >> ~/Documents/lockscreen/lock
-echo "/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend" >> ~/Documents/lockscreen/lock.sh
+echo "/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend" >> ~/Documents/lockscreen/lock
 
 chmod +x ~/Documents/lockscreen/lock
 sudo mv ~/Documents/lockscreen/lock /usr/local/bin
